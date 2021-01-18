6607b270f4ed155584dc12d314c96ef99b20f2fc
old-beans <vincent.cavaliere@gmail.com>
2021-01-17 20:00:52 -0500

    updated with portfolio dictionary and sum, weight functions

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
 
 bal_vinix = 200000
+bal_fzrox = 40000
 bal_fspsx = 70000
 bal_fbtix = 9080
-bal_fzrox = 40000
 
-portfolio = [bal_vinix, bal_fspsx, bal_fbtix, bal_fzrox]
+portfolio = [
+    {'symbol': 'VINIX', 'value': bal_vinix, 'type': 'domestic stock'},
+    {'value': bal_fzrox, 'symbol': 'FZROX', 'type': 'domestic stock'},
+    {'symbol': 'FSPSX', 'type': 'international_stock', 'value': bal_fspsx},
+    {'symbol': 'FBTIX', 'type': 'bond', 'value': bal_fbtix}
+    ]
 
-portfolio_sum = sum(portfolio)
-print("sum = ", portfolio_sum)
 
-for fund in portfolio:
-    fund_weight = int(fund/portfolio_sum * 100)
-    print(fund_weight)
+def sum_portfolio():
+    portfolio_total = sum(fund['value'] for fund in portfolio)
+    print("Total ->", portfolio_total)
+    return portfolio_total
+
+
+def fund_weights(portfolio_total):
+    for fund in portfolio:
+        fund_weight = int(fund['value']/total_value * 100)
+        print(fund['symbol'], '->', fund_weight)
+    return
+
+
+total_value = sum_portfolio()
+fund_weights(total_value)
+
 
 
 
