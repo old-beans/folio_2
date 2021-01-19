4ead5856481a5f4a72c246d5b46bc4e9008db134
old-beans <vincent.cavaliere@gmail.com>
2021-01-18 22:32:28 -0500

    converted functions to pandas dataframe with weighting caluclated

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 bal_vinix = 200000
 bal_fzrox = 40000
@@ -7,30 +8,13 @@ bal_fbtix = 9080
 portfolio = [
     {'symbol': 'VINIX', 'type': 'domestic stock', 'value': bal_vinix},
     {'symbol': 'FZROX', 'type': 'domestic stock', 'value': bal_fzrox},
-    {'symbol': 'FSPSX', 'type': 'international_stock', 'value': bal_fspsx},
-    {'symbol': 'FBTIX', 'type': 'bond', 'value': bal_fbtix}
-    ]
+    {'symbol': 'FSPSX', 'type': 'international stock', 'value': bal_fspsx},
+    {'symbol': 'FBTIX', 'type': 'bond', 'value': bal_fbtix}]
 
 
-def sum_portfolio():
-    portfolio_total = sum(fund['value'] for fund in portfolio)
-    print("Total ->", portfolio_total)
-    return portfolio_total
-
-
-def fund_weights(portfolio_total):
-    total_weight = 0
-    for fund in portfolio:
-        fund_weight = fund['value']/total_value * 100
-        print(fund['symbol'], " -> {:.2f} %".format(fund_weight), "%")
-        total_weight += fund_weight
-
-    print("Total Weight ->", total_weight)
-    return
-
-
-total_value = sum_portfolio()
-fund_weights(total_value)
+df_portfolio = pd.DataFrame(portfolio)
+df_portfolio['weighting'] = (df_portfolio['value']/sum(fund['value'] for fund in portfolio)*100)
+print(df_portfolio)
 
 
 
