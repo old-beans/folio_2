e1aba3148710508a4bb6ec80cd4d9a2b3073c5ed
old-beans <vincent.cavaliere@gmail.com>
2021-01-31 16:19:08 -0500

    "doesn't work - give NaN as a total"

--- main.py
+++ main.py
@@ -5,21 +5,15 @@ bal_fzrox = 40000
 bal_fspsx = 70000
 bal_fbtix = 10000
 
-portfolio = [
-    {'symbol': 'VINIX', 'asset class': 'domestic stock', 'value': bal_vinix},
-    {'symbol': 'FZROX', 'asset class': 'domestic stock', 'value': bal_fzrox},
-    {'symbol': 'FSPSX', 'asset class': 'international stock', 'value': bal_fspsx},
-    {'symbol': 'FBTIX', 'asset class': 'bond', 'value': bal_fbtix}]
+portfolio = {'VINIX': {'asset class': 'domestic stock', 'value': bal_vinix},
+             'FZROX': {'asset class': 'domestic stock', 'value': bal_fzrox},
+             'FSPSX': {'asset class': 'international stock', 'value': bal_fspsx},
+             'FBTIX': {'asset class': 'bond', 'value': bal_fbtix}}
 
-df_portfolio = pd.DataFrame(portfolio)
-df_portfolio['weighting'] = (df_portfolio['value']/sum(fund['value'] for fund in portfolio)*100)
+df_portfolio = pd.DataFrame(portfolio).swapaxes(0, 1)
 df_portfolio.loc["Total"] = df_portfolio.sum(numeric_only=True)
-asset_class_sum = df_portfolio.groupby(['asset class']).sum()
+# asset_class_sum = df_portfolio.groupby(['asset class']).sum()
+
 
 print(df_portfolio)
-print(asset_class_sum)
-
-
-
-
-
+# print(asset_class_sum)
