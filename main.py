514197ba8597f234e7e6b170f1d0050943f7ed51
old-beans <vincent.cavaliere@gmail.com>
2021-02-01 23:10:11 -0500

    minor change

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ bal_vfiax = 5876
 
 portfolio = {'VINIX': {'asset class': 'dom stock', 'p_value': bal_vinix, 'returns': 10, 'Net ER': 0.035},
              'FZROX': {'asset class': 'dom stock', 'p_value': bal_fzrox, 'returns': 10, 'Net ER': 0},
-             'FSPSX': {'asset class': 'int stock', 'p_value': bal_fspsx, 'returns': 5, 'Net ER': 0.035},
-             'VBTIX': {'asset class': 'bond',      'p_value': bal_vbtix, 'returns': 5, 'Net ER': 0.035},
-             'VFIAX': {'asset class': 'dom stock', 'p_value': bal_vfiax, 'returns': 5, 'Net ER': 0.04}}
+             'FSPSX': {'asset class': 'int stock', 'p_value': bal_fspsx, 'returns': 5,  'Net ER': 0.035},
+             'VBTIX': {'asset class': 'bond',      'p_value': bal_vbtix, 'returns': 5,  'Net ER': 0.035},
+             'VFIAX': {'asset class': 'dom stock', 'p_value': bal_vfiax, 'returns': 5,  'Net ER': 0.04}}
 
 df_portfolio = pd.DataFrame.from_dict(portfolio, orient='index')
 df_portfolio = df_portfolio.sort_values('p_value', axis=0, ascending=False)
@@ -26,3 +26,4 @@ asset_class_sum = df_portfolio.groupby(['asset class']).sum()
 asset_class_sum = asset_class_sum.sort_values('weight %', ascending=False)
 
 print(asset_class_sum)
+
